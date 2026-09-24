bb450d7ba449f4920bfa958ed9bbbb35389be24e
Amit Singha <dev.amitsingha@gmail.com>
2026-09-24 22:39:50 +0600

    model create success and relation created

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+    
     protected function casts(): array
     {
         return [
